dd37b6509f95c7973dad108fa765c8c2b7c23f46
ajfabry <ajfabry@sc.rr.com>
2020-04-26 23:36:25 -0400

    Use color to indicate upvote/downvote

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-
 
 import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
     declarations: [VoteComponent],
-    imports: [IonicModule.forRoot()],
+    imports: [CommonModule, IonicModule.forRoot()],
     exports: [VoteComponent, IonicModule]
 })
 
