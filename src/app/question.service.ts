e406b5209dbff4541e92d383ae9f158c96acb1d6
ajfabry <ajfabry@sc.rr.com>
2020-04-27 01:24:01 -0400

    Removed unused method

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -22,30 +22,5 @@ export class Service
         return this.eventSubject;
     }
     
-    async getUsername() {
-        var self = this;
-        var username="Prof";
-        if(firebase.auth().currentUser!=null){
-            console.log("Logged in");
-            console.log(firebase.auth().currentUser.uid);
-            await this.db.collection("username").where("uid", "==", firebase.auth().currentUser.uid)
-            .onSnapshot(function(querySnapshot) {
-                console.log("username loaded");
-                querySnapshot.forEach(function(doc) {
-                    var item = doc.data();
-                    //console.log(item);
-                    username = item.username;
-                    //console.log(username);
-                });
-                // self.events.publish('dataloaded',Date.now());
-                self.publishEvent({
-                    foo: 'bar'
-                });
-                console.log(username)
-                return username
-            } );
-        }
-    }
-    
     constructor() {}
 }
