b638e02cb20eebd810fb2cd81046a37d6120a2de
ajfabry <ajfabry@sc.rr.com>
2020-04-29 20:10:05 -0400

    Filter local to past 24 hours by default

--- src/app/local/local.page.ts
+++ src/app/local/local.page.ts
@@ -41,8 +41,23 @@ export class LocalPage implements OnInit {
 
   ngOnInit() {
     var self = this;
-    let startDate = "0001 01 01";
-    this.service.db.collection("questions").onSnapshot(function(querySnapshot) {
+
+    let startDate = this.cutoffDate || new Date(Date.now() - 24 * 60 * 60 * 1000);
+
+    if (startDate < new Date(Date.now() - 366 * 24 * 60 * 60 * 1000))
+      this.cutoffDisplay = "all time";
+    else if (startDate < new Date(Date.now() - 364 * 24 * 60 * 60 * 1000))
+      this.cutoffDisplay = "past year";
+    else if (startDate < new Date(Date.now() - 29 * 24 * 60 * 60 * 1000))
+        this.cutoffDisplay = "past month";
+    else if (startDate < new Date(Date.now() - 6 * 24 * 60 * 60 * 1000))
+      this.cutoffDisplay = "past week";
+    else if (startDate < new Date(Date.now() - 23 * 60 * 60 * 1000))
+      this.cutoffDisplay = "past day";
+    else
+      this.cutoffDisplay = "past hour";
+
+    this.service.db.collection("questions").where("timestamp", '>', startDate).onSnapshot(function(querySnapshot) {
       self.questions = [];
       var numQuestions = 0;
       querySnapshot.forEach(function(doc) {
@@ -59,9 +74,9 @@ export class LocalPage implements OnInit {
         self.homePage.getQuestionVotes(question).get().then(upvotes => {
           question.votes = sum(values(upvotes.data()));
           self.questions.push(question);
-          // if (self.questions.length == numQuestions) {
-          //   self.sortQuestions();
-          // }
+          if (self.questions.length == numQuestions) {
+            self.questions = self.homePage.sortQuestions(self.questions);
+          }
         });
       });
       this.questions = self.questions;
