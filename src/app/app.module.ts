b15592e3c40f08e9fd5e0f2e4f540658a811d208
Will Heffernan <willheffernan@Wills-MacBook-Pro.local>
2020-04-30 00:31:50 -0400

    Removed the likeness of our glorious leader. Added post history

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,14 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms'
 
+import { HomePage } from './home/home.page';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [
+    HomePage,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/profile-page/profile-page.module.ts
+++ src/app/profile-page/profile-page.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ComponentsModule } from '../components/components.module';
 import { IonicModule } from '@ionic/angular';
 
 import { ProfilePagePageRoutingModule } from './profile-page-routing.module';
@@ -13,6 +13,7 @@ import { ProfilePagePage } from './profile-page.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     ProfilePagePageRoutingModule
   ],
   declarations: [ProfilePagePage]
